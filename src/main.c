d2ffda18ed9442f4140fdb6b20760cd5abacf6be
shiki-01 <0110shiki@gmail.com>
2026-05-18 09:56:12 +0900

    ✨ マウスカーソルアイコンの削除と新規ファイルの追加
    - 不要なマウスカーソルアイコン画像ファイル（mouse_cursor_icon.png）を削除しました。
    - CMakeビルドスクリプト（cmake_rebuild）を新規作成しました。
    - メインプログラム（main.c）を新規作成し、LVGLを使用したウィジェットデモを実装しました。
    - マウスカーソルアイコンを定義するソースファイル（mouse_cursor_icon.c）を新規作成しました。
    - 新しいマウスカーソルアイコン画像ファイル（mouse_cursor_icon.png）を追加しました。

--- src/main.c
+++ src/main.c
@@ -57,34 +57,23 @@ static lv_display_t * hal_init(int32_t w, int32_t h);
 
 int main(int argc, char **argv)
 {
-  /*Initialize LVGL*/
   lv_init();
 
-  /*Initialize the display, and the input devices*/
   hal_init(480, 320);
 
-  /*Open a demo or an example*/
   if (argc <= 1) {
-	/*If not argument if provided just a open a demo or an example.*/
     lv_demo_widgets();
-//      lv_example_chart_1();
   } else {
-	/*Process the command line arguments and open the related demo*/
     if (!lv_demos_create(&argv[1], argc - 1)) {
       lv_demos_show_help();
       goto demo_end;
     }
   }
 
-  /*To hide the memory and performance indicators in the corners
-   *disable `LV_USE_MEM_MONITOR` and `LV_USE_PERF_MONITOR` in `lv_conf.h`*/
-
   while(1) {
-    /* Periodically call the lv_timer_handler. */
     uint32_t time_until_next = lv_timer_handler();
 
     if (time_until_next == LV_NO_TIMER_READY) {
-      /* Simply sleep for a while. */
       time_until_next = LV_DEF_REFR_PERIOD;
     }
 
@@ -100,10 +89,6 @@ demo_end:
  *   STATIC FUNCTIONS
  **********************/
 
-/**
- * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
- * library
- */
 static lv_display_t * hal_init(int32_t w, int32_t h)
 {
   lv_group_set_default(lv_group_create());
@@ -115,11 +100,11 @@ static lv_display_t * hal_init(int32_t w, int32_t h)
   lv_indev_set_display(mouse, disp);
   lv_display_set_default(disp);
 
-  LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
+  LV_IMAGE_DECLARE(mouse_cursor_icon);
   lv_obj_t * cursor_obj;
-  cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
-  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-  lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
+  cursor_obj = lv_image_create(lv_screen_active());
+  lv_image_set_src(cursor_obj, &mouse_cursor_icon);
+  lv_indev_set_cursor(mouse, cursor_obj);
 
   lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
   lv_indev_set_display(mousewheel, disp);
